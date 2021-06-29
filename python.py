fce6de5f92b5c444d6ea072ea69922eebb15ed1d
jhuertahd <77334611+jhuertahd@users.noreply.github.com>
2021-06-29 14:52:45 -0400

    prueba 1

--- python.py
+++ python.py
@@ -1,4 +1,4 @@
-print("Hola mundo desde python")
+print("Hola mundo desde python 1")
 print("La conexión con github ha sido exitosa!")
 print("En hora buena por lograr la conexión a la rama development  !!")
 print("Ultimo Cambio!!!!!!!  ")
